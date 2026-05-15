6161dc2d89dddd2034c51b008edef898612962db
Reisenen <247459983+reisenen@users.noreply.github.com>
2026-05-15 16:43:03 +0300

    add gendiff main script

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+
+    parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
