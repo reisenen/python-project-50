18a6bb48bcecca4ce9fc5b261a69ab551eabb101
Reisenen <247459983+reisenen@users.noreply.github.com>
2026-05-15 21:08:20 +0300

    add plain diff function

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,5 +1,29 @@
 import json
 
 
-first_file = json.load(open('gendiff/files/file1.json'))
-second_file = json.load(open('gendiff/files/file2.json'))
+def generate_diff(first, second):
+    first = json.load(open(first))
+    second = json.load(open(second))
+    diff = {}
+
+    keys = first.keys() | second.keys()
+
+    for key in sorted(keys):
+        if key not in first:
+            diff[f'+ {key}'] = second.get(key)
+        elif key not in second:
+            diff[f'- {key}'] = first.get(key)
+        else:
+            if first[key] == second[key]:
+                diff[key] = first.get(key)
+            else:
+                diff.update({f'- {key}': first[key], f'+ {key}': second[key]})
+    return transform_to_str(diff)
+
+
+def transform_to_str(data):
+    result = []
+    for k, v in data.items():
+        result.append(f'{k}: {v}')
+    string = '\n'.join(result)
+    return '{' + '\n' + string + '\n' + '}'
