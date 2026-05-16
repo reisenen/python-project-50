42d126c77d3511c2bdc4a80f48d4af3d8244b113
Reisenen <247459983+reisenen@users.noreply.github.com>
2026-05-16 05:42:16 +0300

    update render_diff()

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -11,30 +11,45 @@ def generate_diff(file1, file2):
     first_file = load_file(file1)
     second_file = load_file(file2)
 
-    diff = convert_to_str(get_diff(first_file, second_file))
-    return diff
+    return render_diff(get_diff(first_file, second_file))
 
 
 def get_diff(first, second):
     keys = first.keys() | second.keys()
-    result = {}
+    diff = {}
 
     for key in sorted(keys):
         if key not in first:
-            result[get_key(key, '+')] = second[key]
+            diff[key] = {'added': second[key]}
         elif key not in second:
-            result[get_key(key, '-')] = first[key]
+            diff[key] = {'deleted': first[key]}
         elif first[key] == second[key]:
-            result[get_key(key)] = first[key]
+            diff[key] = {'unchanged': first[key]}
         else:
-            result[get_key(key, '-')] = first[key]
-            result[get_key(key, '+')] = second[key]
+            diff[key] = {'changed': {'old': first[key], 'new': second[key]}}
 
-    return result
+    return diff
 
 
-def convert_to_str(coll):
-    items = [f'{key}: {normalize_value(value)}' for key, value in coll.items()]
+def render_diff(diff):
+    output = {}
+
+    for key, value in diff.items():
+        if 'added' in value:
+            output[f'+ {key}'] = value['added']
+        elif 'deleted' in value:
+            output[f'- {key}'] = value['deleted']
+        elif 'unchanged' in value:
+            output[f'  {key}'] = value['unchanged']
+        elif 'changed' in value:
+            output[f'- {key}'] = value['changed']['old']
+            output[f'+ {key}'] = value['changed']['new']
+
+    return convert_to_text(output)
+
+
+def convert_to_text(coll):
+    items = [f'  {key}: {normalize_value(value)}' for key, value in coll.items()]
     result = ['{', *items, '}']
 
     return '\n'.join(result)
@@ -44,12 +59,3 @@ def normalize_value(value):
     if isinstance(value, bool):
         return str(value).lower()
     return value
-
-
-def get_key(key, prefix=None):
-    MAPPING = {
-        '+': f'+ {key}',
-        '-': f'- {key}',
-        'None': f'  {key}',
-    }
-    return MAPPING[prefix] if prefix else MAPPING['None']
