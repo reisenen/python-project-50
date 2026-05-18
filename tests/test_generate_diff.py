d605a9a1c08627653675d2e050df746402149465
Reisenen <247459983+reisenen@users.noreply.github.com>
2026-05-19 02:17:58 +0300

    add yaml diff test

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -11,9 +11,17 @@ def read_file(filename):
     return get_test_data_path(filename).read_text()
 
 
-def test_generate_diff():
+def test_generate_diff_json():
     first_json = get_test_data_path("file1.json")
     second_json = get_test_data_path("file2.json")
     result = read_file("result.txt")
 
     assert generate_diff(first_json, second_json) == result
+
+
+def test_generate_diff_yaml():
+    first_yaml = get_test_data_path("file1.yml")
+    second_yaml = get_test_data_path("file2.yml")
+    result = read_file("result.txt")
+
+    assert generate_diff(first_yaml, second_yaml) == result
