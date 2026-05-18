4552a562d24f11c33613f51524fe511ec9cab5f8
Reisenen <247459983+reisenen@users.noreply.github.com>
2026-05-18 11:33:58 +0300

    add test func

---
+++ tests/test_generate_diff.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from gendiff import generate_diff
+
+
+def get_test_data_path(filename):
+    return Path(__file__).parent / "test_data" / filename
+
+
+def read_file(filename):
+    return get_test_data_path(filename).read_text()
+
+
+def test_generate_diff():
+    first_json = get_test_data_path("file1.json")
+    second_json = get_test_data_path("file2.json")
+    result = read_file("result.txt")
+
+    assert generate_diff(first_json, second_json) == result
