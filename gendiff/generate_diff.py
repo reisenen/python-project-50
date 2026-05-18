3c289ef27e3503913eee70ec481feb457c29f6d6
Reisenen <247459983+reisenen@users.noreply.github.com>
2026-05-19 02:34:10 +0300

    add yaml to load_file()

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,9 +1,15 @@
 import json
+import yaml
 
 
 def load_file(file):
+    _, ext = str(file).split('.')
+
     with open(file) as f:
-        loaded = json.load(f)
+        if ext in ('yml', 'yaml'):
+            loaded = yaml.safe_load(f)
+        if ext in ('json'):
+            loaded = json.load(f)
     return loaded
 
 
