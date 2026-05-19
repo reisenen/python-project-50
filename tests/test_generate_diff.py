8e152c07b1329a2ed81e3a353b34f9c997155cc8
Reisenen <247459983+reisenen@users.noreply.github.com>
2026-05-19 03:36:11 +0300

    add deep test fixtures

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -25,3 +25,19 @@ def test_generate_diff_yaml():
     result = read_file("result.txt")
 
     assert generate_diff(first_yaml, second_yaml) == result
+
+
+def test_generate_diff_json_deep():
+    first_json = get_test_data_path("file1_deep.json")
+    second_json = get_test_data_path("file2_deep.json")
+    result = read_file("result_deep.txt")
+
+    assert generate_diff(first_json, second_json) == result
+
+
+def test_generate_diff_yaml_deep():
+    first_yaml = get_test_data_path("file1_deep.yml")
+    second_yaml = get_test_data_path("file2_deep.yml")
+    result = read_file("result_deep.txt")
+
+    assert generate_diff(first_yaml, second_yaml) == result
