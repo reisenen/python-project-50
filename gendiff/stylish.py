1499a687b032ea9b1b98d0e176dbf2a6c2de2f24
Reisenen <247459983+reisenen@users.noreply.github.com>
2026-07-05 14:31:33 +0300

    refactored stylish

--- gendiff/stylish.py
+++ gendiff/stylish.py
@@ -1,76 +1,94 @@
-def format_dict(items, indent):
-    result = []
-
-    for key, value in items.items():
-        if isinstance(value, dict):
-            result.append(f'{indent}      {key}: {{')
-            result.extend(format_dict(value, indent + '    '))
-            result.append(f'{indent}      }}')
-        else:
-            result.append(f'{indent}      {key}: {normalize(value)}')
-    return result
-
-
 def stylish(diff):
-    def walk(node, depth):
-        output = []
-        indent = ' ' * (depth * 2)
-
-        for key, value in node.items():
-            if value['type'] == 'added':
-                if isinstance(value['value'], dict):
-                    output.append(f'{indent}+ {key}: {{')
-                    output.extend(format_dict(value['value'], indent))
-                    output.append(f'{indent}  }}')
-                else:
-                    output.append(f'{indent}+ {key}: {normalize(value['value'])}')
-
-            elif value['type'] == 'deleted':
-                if isinstance(value['value'], dict):
-                    output.append(f'{indent}- {key}: {{')
-                    output.extend(format_dict(value['value'], indent))
-                    output.append(f'{indent}  }}')
-                else:
-                    output.append(f'{indent}- {key}: {normalize(value['value'])}')
-
-            elif value['type'] == 'unchanged':
-                if isinstance(value['value'], dict):
-                    output.append(f'{indent}  {key}: {{')
-                    output.extend(format_dict(value['value'], indent))
-                    output.append(f'{indent}  }}')
-                else:
-                    output.append(f'{indent}  {key}: {normalize(value['value'])}')
-                
-            elif value['type'] == 'changed':
-                if isinstance(value['old'], dict):
-                    output.append(f'{indent}- {key}: {{')
-                    output.extend(format_dict(value['old'], indent))
-                    output.append(f'{indent}  }}')
-                else:
-                    output.append(f'{indent}- {key}: {normalize(value['old'])}')
-
-                if isinstance(value['new'], dict):
-                    output.append(f'{indent}+ {key}: {{')
-                    output.extend(format_dict(value['new'], indent))
-                    output.append(f'{indent}  }}')
-                else:
-                    output.append(f'{indent}+ {key}: {normalize(value['new'])}')
-
-            else:
-                output.append(f'{indent}  {key}: {{')
-                output.extend(walk(value['children'], depth + 2))
-                output.append(f'{indent}  }}')
-    
-        return output
-    return '\n'.join(['{', *walk(diff, 1), '}'])
+    return '\n'.join(['{', *render_ast(diff), '}'])
 
 
+def render_added(depth, key, value):
+    return render_line(depth, key, value['value'], '+')
 
-def normalize(value):
-    if isinstance(value, bool):
-        return str(value).lower()
-    if value is None:
+
+def render_deleted(depth, key, value):
+    return render_line(depth, key, value['value'], '-')
+
+
+def render_unchanged(depth, key, value):
+    return render_line(depth, key, value['value'], ' ')
+
+
+def render_changed(depth, key, value):
+    old = render_line(depth, key, value['old'], '-')
+    new = render_line(depth, key, value['new'], '+')
+    return [*old, *new]
+
+
+def render_nested(depth, key, value):
+    return render_line(depth, key, value)
+
+
+def render_line(depth, key, value, sign=' '):
+    line = []
+    indent = render_indent(depth)
+
+    prefix = {
+    '+': '+ ',
+    '-': '- ',
+    ' ': '  ',
+    }
+
+    if isinstance(value, dict):
+        line.append(f'{indent}{prefix[sign]}{key}: {{')
+        line.extend(render(value, depth + 1))
+        line.append(f'{indent}  }}')
+    else:
+        line.append(f'{indent}{prefix[sign]}{key}: {render_string(value)}')
+    return line
+
+
+def render(node, depth):
+    if 'type' in node and node['type'] == 'nested':
+        return render_ast(node['children'], depth)
+    return render_json(node, depth)
+
+
+def render_ast(ast, depth=1):
+    render = []
+
+    for key, value in ast.items():
+        get_render = dispatch(value)
+        render.extend(get_render(depth, key, value))
+    return render
+
+ 
+def render_json(json, depth):
+    render = []
+
+    for key, value in json.items():
+        render.extend(render_line(depth, key, value))
+    return render
+
+
+def dispatch(node):
+    HANDLES = {
+        'added': render_added,
+        'deleted': render_deleted,
+        'unchanged': render_unchanged,
+        'changed': render_changed,
+        'nested': render_nested,
+    }
+    node_type = node['type']
+    handler = HANDLES[node_type]
+    return handler
+
+
+def render_indent(depth):
+    INDENT_SIZE = 4
+    return ' ' * (INDENT_SIZE * depth - 2)
+
+
+def render_string(string):
+    if isinstance(string, bool):
+        return str(string).lower()
+    elif string is None:
         return 'null'
-    if not value:
+    elif not string:
         return ''
-    return value
+    return string
